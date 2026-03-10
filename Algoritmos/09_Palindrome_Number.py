2d2f9c7a270bd2f9342067b0107baff0ffb58b00
Asuyz <aemygdioferreira@gmail.com>
2026-03-10 03:30:31 -0300

    Resolução do Algoritmo Num(9) e sua resolução da Lógica

--- Algoritmos/09_Palindrome_Number.py
+++ Algoritmos/09_Palindrome_Number.py
@@ -1,12 +1,18 @@
 class Solution(object):
     def isPalindrome(self, x):
-        x=input(int)
-        
-        
-        
-        
-        """
-        :type x: int
-        :rtype: bool
-        """
-        
+            lista = list(str(x))
+            if lista == list(reversed(lista)):
+                return True
+            else:
+                 return False
+
+
+sol = Solution()
+print(sol.isPalindrome(121))
+
+# Transformamos o número (x) em uma lista e convertemos com (str(x)) 
+# pois o número precisa virar uma string antes do uso
+#
+# Usamos reversed(lista) e não reversed(x) para inverter a lista não o número
+#
+# Depois retornamos o True ou False
