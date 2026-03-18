0d58ab494c2b8a848c7e1b6e8207697bd1402c4e
Asuyz <aemygdioferreira@gmail.com>
2026-03-18 19:40:48 -0300

    Comentarios no exercicio de num 3

--- Algoritmos/03_Longest_Substring_Without_Repeating_Characters.py
+++ Algoritmos/03_Longest_Substring_Without_Repeating_Characters.py
@@ -1,19 +1,21 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        """
-        :type s: str
-        :rtype: int
-        """
+        lista = []
+        count = 0
+
+        for valor_indice in s:
+            if valor_indice in lista
+
+            lista.append(valor_indice)
+
+
+
+                
+
+
+            
+sol = Solution()
+print(sol.lengthOfLongestSubstring
+
         # Criar uma logica que compara se a letra de tras é igual a atual, se for parar a logica, se não for continuar até que aconteça isso.
-        
+        # 3 condicionais for if e while.
